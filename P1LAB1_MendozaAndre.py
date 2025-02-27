5c02a6d9964e7dc5acefdd5dbe06356579462db8
mendozaa8758 <mendozaa8758@student.faytechcc.edu>
2025-02-26 19:43:23 -0500

    Add files via upload

--- P1LAB1_MendozaAndre.py
+++ P1LAB1_MendozaAndre.py
@@ -1,14 +1,8 @@
-Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> # Andre Mendoza
->>> # 2/21/2025
->>> # P1LAB1
->>> # First and Last name, with an intro to the course
->>> 
->>> 
->>> first_name = input("What is your first name? ")
-What is your first name? Andre
->>> last_name = input("What is your last name? ")
-What is your last name? Mendoza
->>> print("Hello", first_name, last_name, "Welcome to CTI-110")
-Hello Andre Mendoza Welcome to CTI-110
+# Andre Mendoza
+# 2/26/2025
+# P1LAB1
+# First and Last name, with an introduction to CTI-110
+
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+print("Hello there", first_name, last_name, "Welcome to CTI-110!")
